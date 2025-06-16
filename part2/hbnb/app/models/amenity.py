b2f5ea1095b256833022b213cad8bcf3277ceed8
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-16 16:12:08 +0200

    Remove name attribution in entry parameters of init method

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,8 +1,8 @@
 from app.models.base_model import BaseModel
 
 class Amenity(BaseModel):
-    def __init__(self, name=""):
-        super().__init()
+    def __init__(self, name):
+        super().__init__()
         self.name = name
     
     @property
