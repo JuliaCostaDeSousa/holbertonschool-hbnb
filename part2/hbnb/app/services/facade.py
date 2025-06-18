05eb6bcd5870b6db9adbbe434210315896a0e095
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-18 18:27:01 +0200

    Writing amenity CRUD methods (create, get, get_all, update) in facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,4 +1,8 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -7,12 +11,25 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder method for creating a user
-    def create_user(self, user_data):
-        # Logic will be implemented in later tasks
-        pass
+    def create_amenity(self, amenity_data):
+        new_amenity = Amenity(name=amenity_data['name'])
+        self.amenity_repo.add(new_amenity)
+        return new_amenity.to_dict()
+    
+    def get_amenity(self, amenity_id):
+        amenity = self.amenity_repo.get_by_attribute('id', amenity_id)
+        if amenity is None:
+            raise ValueError("Amenity not found")
+        return amenity.to_dict()
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
+    def get_all_amenities(self):
+        amenities = self.amenity_repo.get_all()
+        return [amenity.to_dict() for amenity in amenities]
+
+    def update_amenity(self, amenity_id, amenity_data):
+        amenity = self.amenity_repo.get_by_attribute('id', amenity_id)
+        if amenity is None:
+            raise ValueError("Amenity not found")
+        
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return  self.amenity_repo.get(amenity_id).to_dict()
