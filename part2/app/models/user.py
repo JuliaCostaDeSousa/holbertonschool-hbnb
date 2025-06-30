af34d5fb9d43733e1143df8459d43edeb8fe1a67
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-30 13:35:31 +0200

    Update User class to include a password attribute and to add hash_password and verify_password methods

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -4,11 +4,12 @@ import re
 class User(BaseModel):
     emails = set()
 
-    def __init__(self, first_name, last_name, email, is_admin=False):
+    def __init__(self, first_name, last_name, email, password, is_admin=False):
         super().__init__()
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.password = password
         self.is_admin = is_admin
         self.places = []
         self.reviews = []
@@ -81,3 +82,13 @@ class User(BaseModel):
             'last_name': self.last_name,
             'email': self.email
         }
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        from app import bcrypt
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def verify_password(self, password):
+        """Verifies if the provided password matches the hashed password."""
+        from app import bcrypt
+        return bcrypt.check_password_hash(self.password, password)
