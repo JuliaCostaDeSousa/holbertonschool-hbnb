13e8b9d9865bc5a7754b663964eda0fecbb2495a
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-01 10:30:37 +0200

    Import flask_alchemy and initialization of database

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from app.api.v1.users import api as users_ns
@@ -7,11 +8,13 @@ from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
 bcrypt = Bcrypt()
+db = SQLAlchemy()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
+    db.init_app(app)
     api = Api(app, version='1.0', title='HBnB Auth and DB', description='HBnB Authentication and Database')
 
     api.add_namespace(users_ns, path='/api/v1/users')
