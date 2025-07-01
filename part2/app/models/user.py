1137590f48e2e068bc5703a421826541c772b6d2
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-01 14:37:25 +0200

    Map User attributes to database columns and add validation decorators

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,67 +1,46 @@
 from .basemodel import BaseModel
 import re
+from app import db, bcrypt
+import uuid
+from sqlalchemy.orm import validates
 
 class User(BaseModel):
     emails = set()
 
-    def __init__(self, first_name, last_name, email, password, is_admin=False):
-        super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-        self.is_admin = is_admin
-        self.places = []
-        self.reviews = []
-    
-    @property
-    def first_name(self):
-        return self.__first_name
-    
-    @first_name.setter
-    def first_name(self, value):
+    __tablename__ = 'users'
+
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+
+    @validates('first_name')
+    def validates_first_name(self, key, value):
         if not isinstance(value, str):
-            raise TypeError("First name must be a string")
-        super().is_max_length('First name', value, 50)
-        self.__first_name = value
+            raise TypeError("{} must be a string".format(key))
+        return value.strip()
 
-    @property
-    def last_name(self):
-        return self.__last_name
-
-    @last_name.setter
-    def last_name(self, value):
+    @validates('last_name')
+    def validates_last_name(self, key, value):
         if not isinstance(value, str):
-            raise TypeError("Last name must be a string")
-        super().is_max_length('Last name', value, 50)
-        self.__last_name = value
+            raise TypeError("{} must be a string".format(key))
+        return value.strip()
 
-    @property
-    def email(self):
-        return self.__email
-
-    @email.setter
-    def email(self, value):
+    @validates('email')
+    def validates_email(self, key, value):
         if not isinstance(value, str):
-            raise TypeError("Email must be a string")
+            raise TypeError("{} must be a string".format(key))
         if not re.match(r"[^@]+@[^@]+\.[^@]+", value):
             raise ValueError("Invalid email format")
-        if value in User.emails:
-            raise ValueError("Email already exists")
-        if hasattr(self, "_User__email"):
-            User.emails.discard(self.__email)
-        self.__email = value
-        User.emails.add(value)
+        return value.strip()
 
-    @property
-    def is_admin(self):
-        return self.__is_admin
-    
-    @is_admin.setter
-    def is_admin(self, value):
+    @validates('is_admin')
+    def validates_is_admin(self, key, value):
         if not isinstance(value, bool):
-            raise TypeError("Is Admin must be a boolean")
-        self.__is_admin = value
+            raise TypeError("{} must be a boolean".format(key))
+        return value
 
     def add_place(self, place):
         """Add an amenity to the place."""
@@ -85,10 +64,8 @@ class User(BaseModel):
 
     def hash_password(self, password):
         """Hashes the password before storing it."""
-        from app import bcrypt
         self.password = bcrypt.generate_password_hash(password).decode('utf-8')
 
     def verify_password(self, password):
         """Verifies if the provided password matches the hashed password."""
-        from app import bcrypt
         return bcrypt.check_password_hash(self.password, password)
