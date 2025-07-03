0bcc1c21e6ef165bc88ea8b91fa6eaca651eb07f
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-03 19:11:31 +0200

    Map the Amenity entity to SQLAlchemy model

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,28 +1,28 @@
 from .basemodel import BaseModel
+from app.extensions import db
+from sqlalchemy.orm import validates
+
 
 class Amenity(BaseModel):
-	def __init__(self, name):
-		super().__init__()	
-		self.name = name
+    __tablename__ = 'amenities'
 
-	@property
-	def name(self):
-		return self.__name
+    id = db.Column(db.String(36), primary_key=True, nullable=False, unique=True)
+    name = db.Column(db.String(50), nullable=False)
 
-	@name.setter
-	def name(self, value):
-		if not isinstance(value, str):
-			raise TypeError("Name must be a string")
-		if not value:
-			raise ValueError("Name cannot be empty")
-		super().is_max_length('Name', value, 50)
-		self.__name = value
+    @validates('name')
+    def validates_name(self, key, value):
+        if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+        if not value:
+            raise ValueError("Name cannot be empty")
+        super().is_max_length('Name', value, 50)
+        return value.strip()
 
-	def update(self, data):
-		return super().update(data)
+    def update(self, data):
+        return super().update(data)
 	
-	def to_dict(self):
-		return {
-			'id': self.id,
-			'name': self.name
-		}
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
