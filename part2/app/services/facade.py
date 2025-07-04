47b6628c9be193ea966f23c2b339778f255a362b
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-04 10:57:12 +0200

    Update the facade to use the newly created AmenityRepository class, ReviewRepository class and PlaceRepository class

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,8 @@
 from app.persistence.repository import SQLAlchemyRepository
 from app.persistence.user_repository import UserRepository
+from app.persistence.place_repository import PlaceRepository
+from app.persistence.amenity_repository import AmenityRepository
+from app.persistence.review_repository import ReviewRepository
 
 from app.models.user import User
 from app.models.amenity import Amenity
@@ -9,9 +12,9 @@ from app.models.review import Review
 class HBnBFacade:
     def __init__(self):
         self.user_repo = UserRepository()
-        self.amenity_repo = SQLAlchemyRepository(Amenity)
-        self.place_repo = SQLAlchemyRepository(Place)
-        self.review_repo = SQLAlchemyRepository(Review)
+        self.amenity_repo = AmenityRepository()
+        self.place_repo = PlaceRepository()
+        self.review_repo = ReviewRepository()
 
     # USER
     def create_user(self, user_data):
