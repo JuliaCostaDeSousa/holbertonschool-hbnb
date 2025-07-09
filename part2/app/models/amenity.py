d745ed653d748083d9e6490f4d954e50df8709c3
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-09 10:28:57 +0200

    Update name as unique to avoid duplicate entry

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import validates
 class Amenity(BaseModel):
     __tablename__ = 'amenities'
 
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=True)
 
     @validates('name')
     def validates_name(self, key, value):
